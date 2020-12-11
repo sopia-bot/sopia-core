caa9717492117039fa2514a158624fcd5e3cc092
TreeSome <youn@tree-some.dev>
2020-12-11 16:58:53 +0900

    for legacy getters

--- src/struct/struct.ts
+++ src/struct/struct.ts
@@ -6,6 +6,13 @@
  */
 import { serialize, deserialize } from 'typescript-json-serializer';
 
+const snakeToCamel = (str: string) => str.replace(
+    /([-_][a-z])/g,
+    (group) => group.toUpperCase()
+                    .replace('-', '')
+                    .replace('_', '')
+);
+
 export class Struct {
 	static deserialize(data: any) {
 		return deserialize(data, this);
@@ -13,5 +20,19 @@ export class Struct {
 
 	public serialize(): any {
 		return serialize(this, true);
+	}
+
+	constructor() {
+		return new Proxy(this, this);
+	}
+
+	public get(struct: any, name: string, proxy: any) {
+		const anyThis = this as any;
+		if ( anyThis[name] ) {
+			return anyThis[name];
+		}
+
+		const t = snakeToCamel(name);
+		return anyThis[t];
 	}
 }
